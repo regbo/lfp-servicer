76e0a140f4b981bf31c240193b471d8c0e1b6c2f
austinv11 <austinv11@gmail.com>
2020-05-23 21:13:15 -0400

    Fix handling of multiple service files.

--- src/main/java/com/austinv11/servicer/ServicerProcessor.java
+++ src/main/java/com/austinv11/servicer/ServicerProcessor.java
@@ -11,8 +11,8 @@ import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 import javax.tools.FileObject;
 import javax.tools.StandardLocation;
-import java.io.IOException;
-import java.io.Writer;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
@@ -57,19 +57,48 @@ public class ServicerProcessor extends AbstractProcessor {
                 }
             }
         }
-        messager.printMessage(Diagnostic.Kind.NOTE, "Found " + services.size() + " services!");
+
+        messager.printMessage(Diagnostic.Kind.NOTE, "Found " + services.size() + " services!\n");
+
         services.forEach((k, v) -> {
+            String serviceLocation = "META-INF/services" + "/" + k;
+
+            List<String> oldServices = new ArrayList<>();
+
             try {
-                FileObject fo = filer.createResource(StandardLocation.CLASS_OUTPUT,
-                        "", "META-INF/services" + "/" + k);
-                try (Writer w = fo.openWriter()) {
+                // If the file has already been created, we must first call getResource to allow for overwriting
+                FileObject fo = filer.getResource(StandardLocation.CLASS_OUTPUT, "", serviceLocation);
+
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(fo.openInputStream(),
+                        StandardCharsets.UTF_8))) { // Can't check if it exists, must catch an exception from here
+
+                    reader.lines()
+                            .map(line -> {
+                                int comment = line.indexOf("#");
+
+                                return (comment >= 0 ? line.substring(0, comment) : line).trim();
+                            })
+                            .filter(line -> !line.isEmpty())
+                            .forEach(oldServices::add);
+                }
+            } catch (Exception e) {
+                messager.printMessage(Diagnostic.Kind.NOTE, serviceLocation + " does not yet exist!\n");
+            }
+
+            try {
+                FileObject fo = filer.createResource(StandardLocation.CLASS_OUTPUT, "", serviceLocation);
+                try (OutputStreamWriter w = new OutputStreamWriter(fo.openOutputStream())) {
+                    for (String oldService : oldServices) {
+                        w.append(oldService).append("\n");
+                    }
                     for (String impl : v) {
-                        messager.printMessage(Diagnostic.Kind.NOTE, "Setting up " + impl + " for use as a " + k + " implementation!");
+                        messager.printMessage(Diagnostic.Kind.NOTE, "Setting up " + impl + " for use as a " + k + " implementation!\n");
                         w.append(impl).append("\n");
                     }
                 }
             } catch (IOException e) {
                 messager.printMessage(Diagnostic.Kind.ERROR, e.getMessage());
+                e.printStackTrace();
             }
         });
         return true;
